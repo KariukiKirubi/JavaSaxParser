e38a50ce0ee5cc344446ab996d8c61d213f726bd
Kariuki Kirubi <k.k.kirubi@kisiiuniversity.ac.ke>
2019-11-26 10:34:59 +0300

    Modified the setLego class

--- PatelAssignment6/src/patelAssign6/LegoSet.java
+++ PatelAssignment6/src/patelAssign6/LegoSet.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package patelAssign6;
 
-/**
- *
- * @author karis
- */
 public class LegoSet {
-    
+
+       private String name;
+       private String setNum;
+       
+       public LegoSet() {
+       }
+
+       public LegoSet(String name, String setNum) {
+              this.name = name;
+              this.setNum = setNum;
+       }
+
+       public String getName() {
+              return name;
+       }
+
+       public void setName(String name) {
+              this.name = name;
+       }
+
+       public String getsetNum() {
+              return setNum;
+       }
+
+       public void setsetNum(String setNum) {
+              this.setNum = setNum;
+       }
+
+       public String toString() {
+              StringBuffer sb = new StringBuffer();
+              
+              sb.append("Name:" + getName());
+              sb.append(", ");
+              sb.append("Set Num:" + getsetNum());
+              sb.append(", ");
+             
+              return sb.toString();
+       }
 }
+
